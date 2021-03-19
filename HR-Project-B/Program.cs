c5051aaacca60e91b4b4b9e89396179280ea59cc
Karan2002-tj <74537794+Karan2002-tj@users.noreply.github.com>
2021-03-19 13:32:31 +0100

    role menu finish

--- HR-Project-B/Program.cs
+++ HR-Project-B/Program.cs
@@ -1,17 +1,20 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
 
 namespace HR_Project_B
 {
     class Program
     {
-        static void Main(string[] args)
+        
+
+        private static void Main(string[] args)
         {
-
             Register.Start();
-            Console.WriteLine("Hello World!");
-
             Karan.Start();
-
 
         }
     }
