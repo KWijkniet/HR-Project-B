a92085cd7b6f698540433a6600cb86cf688cd564
Kelvin Wijkniet <1006099@hr.nl>
2021-04-02 12:43:51 +0200

    Update merge

--- HR-Project-B/UserManager.cs
+++ HR-Project-B/UserManager.cs
@@ -18,13 +18,12 @@ namespace HR_Project_B
             }
             options[^1] = "Back";
 
-
             while (true)
             {
                 Console.Clear();
                 OptionMenu menu = new OptionMenu(titel, options);
                 int response = menu.Display();
-                if (response == options.Length)
+                if (response == options.Length - 1)
                 {
                     return;
                 }
@@ -73,59 +72,25 @@ namespace HR_Project_B
 
                 }
 
-
-
                 while (true)
                 {
                     Console.Clear();
-                    TextTool.TextColor($"Name: {selectUser.name}\nEmail: {selectUser.email}\nPhone: {selectUser.phone}\nRole: {roleName}\n", ConsoleColor.Gray, true);
-                    TextTool.TextColor("Select a role number:\n1 = customer \n2 = chef \n3 = manager \n4 = admin", ConsoleColor.Green, true);
-                    string tempRole = Console.ReadLine();
-                    if (tempRole.Length > 0)
+                    string userInfo = $"Name: {selectUser.name}\nEmail: {selectUser.email}\nPhone: {selectUser.phone}\nRole: {roleName}\n\nSelect a role:";
+
+                    OptionMenu roleSelector = new OptionMenu(userInfo, new string[] { "Customer", "Chef", "Manager", "Admin", });
+                    int index = roleSelector.Display();
+                    selectUser.role = index + 1;
+                    for (int i = 0; i < Program.accounts.Length; i++)
                     {
-                        try
+                        if (Program.accounts[i].id == selectUser.id)
                         {
-                      
-                            int temp = int.Parse(tempRole);
-                            if(temp < 1 || temp > 4)
-                            {
-                                throw new Exception();
-                            
-                            }
-                            selectUser.role = temp;
-                            for (int i = 0; i < Program.accounts.Length; i++)
-                            {
-                                if(Program.accounts[i].id == selectUser.id)
-                                {
-                                    Program.accounts[i].role = selectUser.role;
-
-                                }
-
-                            }
-                            Program.SaveAccounts();
-                            break;
-                        }
-                        catch (Exception)
-                        {
-                            TextTool.TextColor("Please enter a valid role!", ConsoleColor.Red, true);
-                            Console.ReadKey();
+                            Program.accounts[i].role = selectUser.role;
                         }
                     }
-                    else
-                    {
-                        TextTool.TextColor("Please enter a valid role!", ConsoleColor.Red, true);
-                        Console.ReadKey();
-                    }
+                    Program.SaveAccounts();
+                    break;
                 }
-
-
             }
-
-        
-        
         }
-
-
-
     }
 }
