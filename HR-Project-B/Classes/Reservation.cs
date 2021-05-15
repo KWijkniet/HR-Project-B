a51328a72f3def57e5be58a6b835ef25b06ddf5a
Karan2002-tj <74537794+Karan2002-tj@users.noreply.github.com>
2021-05-15 15:55:17 +0200

    reservation class

--- HR-Project-B/Classes/Reservation.cs
+++ HR-Project-B/Classes/Reservation.cs
@@ -2,14 +2,40 @@
 using System.Collections.Generic;
 using System.Text;
 
-namespace HR_Project_B.Classes
+namespace HR_Project_B
 {
     class Reservation
     {
         public string creditcardNumber;
         public string orderType; //take-away || table reservation
         public string orderID; //unique
+        public string tableID;
+        public string userID;
+        public string userName;
+        public string userEmail;
 
+        public Reservation(string creditcardNumber, string orderType, string tableID, string userID, string userName, string userEmail)
+        {
+            this.creditcardNumber = creditcardNumber;
+            this.orderType = orderType;
+            this.orderID = Guid.NewGuid().ToString();
+            this.tableID = tableID;
+            this.userID = userID;
+            this.userName = userName;
+            this.userEmail = userEmail;
+
+    }
+
+        public Reservation(dynamic data)
+        {
+            this.creditcardNumber = data.creditcardNumber;
+            this.orderType = data.orderType;
+            this.orderID = data.orderID;
+            this.tableID = data.tableID;
+            this.userID = data.userID;
+            this.userName = data.userName;
+            this.userEmail = data.userEmail;
+        }
         /*
         id(unique key)
         account_id
@@ -20,3 +46,5 @@ namespace HR_Project_B.Classes
         */
     }
 }
+
+
